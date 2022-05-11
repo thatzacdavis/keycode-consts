a76230185e336827c88272010e0362563ab50f71
Zac Davis <thatzacdavis@gmail.com>
2022-05-11 08:35:36 -0400

    Add known key values to KeyCodes

--- src/classes/keyCode.js
+++ src/classes/keyCode.js
@@ -1,10 +1,11 @@
 class KeyCode {
-    constructor(HexValue, NumericValue, ShortDescription, LongDescription = '') {
+    constructor(HexValue, NumericValue, ShortDescription, LongDescription = '', Key = '') {
         this.HexValue = HexValue;
         this.KeyCode = NumericValue;
         this.NumericValue = NumericValue;
         this.ShortDescription = ShortDescription;
         this.LongDescription = LongDescription;
+        this.Key = Key;
     }
 }
 
